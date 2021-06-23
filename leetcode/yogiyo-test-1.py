df2935f75e05a6351e64a9047d2e4ca4f7da2fe6
rincher <skymegod@gmail.com>
2021-06-23 14:41:28 +0900

    yogiyo

---
+++ leetcode/yogiyo-test-1.py
@@ -0,0 +1,30 @@
+def check_duplicate(Email_name, Email):
+    i = 1
+    if Email_name in Email:
+        Email_name = Email_name+str(i+1)
+        if Email_name+str(2) in Email_name:
+            i = 2
+            Email_name = Email_name+str(i+1)
+    return Email_name
+
+
+def solution(S, C):
+    # write your code in Python 3.6
+    Names = S.split(", ")
+    Email = ""
+    Email_names = ""
+    i = 1
+    for name in Names:
+        First_name = name.split(" ")[0].lower()
+        Last_name = name.split(" ")[-1].lower()
+        Last_name = Last_name.replace("-", "")
+        Email_name = First_name + "." + Last_name
+        Email_name = check_duplicate(Email_name, Email)
+        Email += name + " " + "<" + Email_name + "@" + C.lower() + ".com" + ">, "
+    return Email
+
+
+C = "Example"
+S = "John Doe, Peter Benjamin Parker, Mary Jane Watson-Parker, John Elvis Doe, John Evan Doe, Jane Doe, Peter Brian Parker"
+
+print(solution(S, C))
