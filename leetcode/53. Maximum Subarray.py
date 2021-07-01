079660e8add487b0885e1756361a2918a2be2c14
rincher <skymegod@gmail.com>
2021-07-01 17:58:07 +0900

    53rd Question. RunTime improvement.

--- leetcode/53. Maximum Subarray.py
+++ leetcode/53. Maximum Subarray.py
@@ -3,13 +3,14 @@ def maxSubArray(nums):
         return nums[0]
     sum1 = min(nums)
     sum2 = 0
-    for i in range(len(nums)):
+    # instead of using range, adding the numbers directly has improved the runtime in leetcode
+    for i in nums:
         if sum2 < 0:
             sum2 = 0
-        sum2 += nums[i]
+        sum2 += i
         sum1 = max(sum1, sum2)
     return sum1
 
 
-nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+nums = [5, 4, -1, 7, 8]
 print(maxSubArray(nums))
