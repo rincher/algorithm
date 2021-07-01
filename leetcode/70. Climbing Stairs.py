584a54d0f53e3043d2bdb2bfb59873b81921679a
rincher <skymegod@gmail.com>
2021-07-02 00:07:52 +0900

    70th Question. Using Meomization and recursive function

--- leetcode/70. Climbing Stairs.py
+++ leetcode/70. Climbing Stairs.py
@@ -1,14 +1,17 @@
-def ClimbStairs(n):
-    if n <= 2:
+cache = {}
+
+
+def memoization(n):
+    if n not in cache.keys():
+        cache[n] = climbStairs(n)
+    return cache[n]
+
+
+def climbStairs(n):
+    if n <= 3:
         return n
-    else:
-        res, step_1, step_2 = 0, 1, 2
-        for i in range(2, n):
-            res = step_2 + step_1
-            step_1 = step_2
-            step_2 = res
-        return res
+    return memoization(n-1) + memoization(n-2)
 
 
-n = 6
-print(ClimbStairs(n))
+n = 5
+print(climbStairs(n))
